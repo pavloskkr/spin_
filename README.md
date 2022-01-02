ec39529d8d5d2339fdff9a6f48a819fc3d191571
pavloskkr <pavloskkr1@hotmail.com>
2022-01-02 06:16:15 +0200

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # spin_
 A real estate portal's search service
+
+# **Instructions:**
+
+1. Download the source code from [here](https://github.com/pavloskkr/spin_/archive/refs/heads/main.zip)
+2. 
+
--- src/main/java/com/example/spin_/controllers/MainController.java
+++ src/main/java/com/example/spin_/controllers/MainController.java
@@ -4,6 +4,8 @@ import com.example.spin_.model.Estate;
 import com.example.spin_.model.JoinedEstateTypes;
 import com.example.spin_.repositories.impl.EstateRepoImpl;
 import com.example.spin_.repositories.impl.TypeRepoImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +17,8 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping(path = "api/v1/S2")
 public class MainController<allAvailableTypes> {
+
+    Logger logger = LoggerFactory.getLogger(MainController.class);
 
     private final EstateRepoImpl estateRepoImpl;
     private final TypeRepoImpl typeRepoImpl;
@@ -28,11 +32,13 @@ public class MainController<allAvailableTypes> {
         this.typeRepoImpl = typeRepoImpl;
     }
 
-    @GetMapping
     public List<Estate> getEstate() {
         List<JoinedEstateTypes> finalMappedTypesList = new ArrayList<>();
         List<Estate> allAvailabeEstates = new ArrayList<>();
 
+        /* Joined type names to object estate and storing it into map
+           Converted the map into object list
+           Filling the initial list with the updated type names */
         allAvailableTypes = typeRepoImpl.getAllJoinedEstates();
         typesMap = allAvailableTypes.stream().collect(Collectors.toMap(JoinedEstateTypes::geteId, JoinedEstateTypes::getTypeName, (typeX,typeY) -> typeX + "," + typeY));
         allAvailabeEstates = estateRepoImpl.getEstate();
@@ -50,18 +56,22 @@ public class MainController<allAvailableTypes> {
         return allAvailabeEstates;
     }
 
+    // Retrieving all available estates joined by type name
     public List<JoinedEstateTypes> getAllJoinedEstates() {
         return  typeRepoImpl.getAllJoinedEstates();
     }
 
+    // Retrieving all available locations
     public List<String> getAllAvailableLocations() {
         return estateRepoImpl.getAllAvailableLocations();
     }
 
+    // Retrieving the availability
     public List<String> getAllAvailableAvailabilityTypes() {
         return estateRepoImpl.getAllAvailableAvailabilityTypes();
     }
 
+    // Retrieving all available distinct types of estate
     public List<String> getAllDistinctTypes() {
         return typeRepoImpl.getAllDistinctTypes();
     }
@@ -69,19 +79,19 @@ public class MainController<allAvailableTypes> {
     @Controller
     public class HomeController {
 
-
-
+        // Loading the home page with its elements filled in
         @GetMapping(path = "/index")
         public String getIndex(Model model) {
 
             model.addAttribute("locations", getAllAvailableLocations());
             model.addAttribute("availabilityTypes",getAllAvailableAvailabilityTypes());
             model.addAttribute("types", getAllDistinctTypes());
-            model.addAttribute("estates", getEstate()); //to bring the concatenated types
+            model.addAttribute("estates", getEstate()); //retrieving the concatenated types
 
             return "index";
         }
 
+        // Perform the search according to user's filters
         @GetMapping(path = "/search")
         public String makeSearch(Model model,
                           @RequestParam(name = "priceFrom",required = false) Integer priceFrom,
@@ -95,7 +105,7 @@ public class MainController<allAvailableTypes> {
             model.addAttribute("locations", getAllAvailableLocations());
             model.addAttribute("availabilityTypes",getAllAvailableAvailabilityTypes());
             model.addAttribute("types", getAllDistinctTypes());
-            model.addAttribute("estates", getEstate()); //to bring the concatenated types
+            model.addAttribute("estates", getEstate()); //retrieving the concatenated types
 
             if (priceFrom == null ) {
                 priceFrom = 10;
@@ -116,8 +126,22 @@ public class MainController<allAvailableTypes> {
                 type = typeRepoImpl.getAllDistinctTypes();
             }
 
-            model.addAttribute("estates",estateRepoImpl.getEstatesByFilters(priceFrom,priceTo,squareFrom,squareTo,
-                    location,type,availability));
+            logger.info("===================== User selections ==============================");
+            logger.info("User selected priceFrom : " + priceFrom);
+            logger.info("User selected priceTo : " + priceTo);
+            logger.info("User selected squareFrom : " + squareFrom);
+            logger.info("User selected squareTo : " + squareTo);
+            logger.info("User selected location : " + location);
+            logger.info("User selected type : " + type);
+            logger.info("User selected availability : " + availability);
+            logger.info("=======================================================================");
+
+            try {
+                // Query according to user's selections.
+                model.addAttribute("estates",estateRepoImpl.getEstatesByFilters(priceFrom,priceTo,squareFrom,squareTo,location,type,availability));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
             return "index";
         }
